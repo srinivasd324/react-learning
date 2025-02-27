84ef0078fed99a074686734210954bac6bfa9b43
srinivasd324 <srinivasd324@gmail.com>
2025-02-27 10:33:15 +0530

    Modified Custom Hook

--- 18 Practice Project - Food Order/01-starting-project/src/hooks/useHttp.js
+++ 18 Practice Project - Food Order/01-starting-project/src/hooks/useHttp.js
@@ -1,3 +1,24 @@
+import { useState } from "react";
+
+async function sendHttpRequest(url, config){
+    const response = await fetch(url, config);
+    const data = await response.data();
+
+    if(!response.ok){
+        throw new Error(data.message || "Something went wrong...Failed to send equest");
+    }
+
+    return data;
+}
 export default function useHttp(){
+    useState();
+    function sendRequest(){
+        try{
+            const resData = sendHttpRequest();
+        } catch(error){
+            
+        }
+        
+    }
     
 }
